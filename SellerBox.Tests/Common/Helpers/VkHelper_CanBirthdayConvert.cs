cdfd1363ead6374b3e7511077249980c1551f1e1
azrael <azrael141@gmail.com>
2018-11-07 00:11:53 +0300

    VkHelper.Tests

---
+++ SellerBox.Tests/Common/Helpers/VkHelper_CanBirthdayConvert.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xunit;
+
+namespace SellerBox.Tests.Common.Helpers
+{
+    public class VkHelper_CanBirthdayConvert
+    {
+        [Theory]
+        [InlineData(null)]
+        [InlineData("")]
+        public void ReturnNullOnEmptyString(string birthday)
+        {
+            var result = SellerBox.Common.Helpers.VkHelper.BirtdayConvert(birthday);
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData("12102018")]
+        [InlineData("12.102018")]
+        [InlineData("32.13")]
+        public void ReturnNullOnInvalidString(string birthday)
+        {
+            var result = SellerBox.Common.Helpers.VkHelper.BirtdayConvert(birthday);
+            Assert.Null(result);
+        }
+
+        [Theory]
+        [InlineData("12.10.2018")]
+        [InlineData("12.10")]
+        public void ReturnDateOnCorrectString(string birthday)
+        {
+            var result = SellerBox.Common.Helpers.VkHelper.BirtdayConvert(birthday);
+            Assert.NotNull(result);
+            Assert.IsType<DateTime>(result);
+            Assert.Equal(new DateTime(2018, 10, 12), result);
+        }
+    }
+}
--- SellerBox/Common/Helpers/VkHelper.cs
+++ SellerBox/Common/Helpers/VkHelper.cs
@@ -9,9 +9,12 @@ namespace SellerBox.Common.Helpers
             if (string.IsNullOrWhiteSpace(birthday))
                 return null;
             string[] dateParts = birthday.Split('.');
-            if (DateTime.TryParseExact(birthday, new string[] { "d.M.yyyy", "d.M" }, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime result))
+            if (dateParts.Length < 2)
+                return null;
+
+            if (DateTime.TryParseExact(birthday, new string[] { "d.M.yyyy", "d.M", "dd.MM" }, System.Globalization.CultureInfo.InvariantCulture, System.Globalization.DateTimeStyles.None, out DateTime result))
             {
-                if (dateParts.Length == 2 && result.Year == DateTime.UtcNow.Year)
+                if (dateParts.Length == 2 && result.Year == 1)
                     return result.AddYears(-result.Year + 1);
                 return result;
             }
